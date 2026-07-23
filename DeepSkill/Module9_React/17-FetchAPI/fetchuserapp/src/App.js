99881fb6edff36b1f0a329224615a0b3b17d193a
Rahul Singh <rahulsingh936805@gmail.com>
2026-07-23 15:03:09 +0530

    Add DeepSkill\Module9_React\17-FetchAPI\fetchuserapp\src\App.js

---
+++ DeepSkill/Module9_React/17-FetchAPI/fetchuserapp/src/App.js
@@ -0,0 +1,19 @@
+import './App.css';
+
+import Getuser from "./Components/Getuser";
+
+function App() {
+
+  return (
+
+      <div className="App">
+
+        <Getuser/>
+
+      </div>
+
+  );
+
+}
+
+export default App;
